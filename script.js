db74955f505f4ae9e4b80b8a9283aa8988408389
Freework-site <72634873+Freework-site@users.noreply.github.com>
2021-11-21 04:01:56 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Add to Card Functionality
+var add = document.getElementById("add");
+var quantity = document.getElementById("quantity").value;
+add.addEventListener('click', () => {
+    console.log("")
+})
+
+
+function changeImageSrc(anything)
+{
+    document.querySelector('.mask').src = anything;
+}
